f3349e8e1bf643c40a7780f70279c76c63d454db
Ilya Naydanov <turbcool@gmail.com>
2022-05-26 17:20:36 +0500

    Docker: each app = own container; single-spa

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,9 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t singlespasample/postgre-sql ../SQL
 
-docker build --no-cache -f Dockerfile -t singlespasample/app ../..
+docker build --no-cache -f root-app\Dockerfile -t singlespasample/root-app ../..
+
+docker build --no-cache -f ember-app\Dockerfile -t singlespasample/ember-app ../..
+
+docker build --no-cache -f react-app\Dockerfile -t singlespasample/react-app ../..
+
+docker build --no-cache -f backend\Dockerfile -t singlespasample/backend ../..
