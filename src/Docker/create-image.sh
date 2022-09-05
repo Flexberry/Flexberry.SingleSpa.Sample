b165c5cb15f3f133f5c96390a2adec9bea9830ac
Ilya Naydanov <turbcool@gmail.com>
2022-09-05 21:52:48 +0500

    update create-image.sh

--- src/Docker/create-image.sh
+++ src/Docker/create-image.sh
@@ -1,5 +1,6 @@
 docker build -f SQL/Dockerfile.PostgreSql -t singlespasample/postgre-sql ../SQL
 docker build -f root-app/Dockerfile -t singlespasample/root-app ../..
 docker build -f ember-app/Dockerfile -t singlespasample/ember-app ../..
+docker build -f ember-app-navbar/Dockerfile -t singlespasample/ember-app-navbar ../..
 docker build -f react-app/Dockerfile -t singlespasample/react-app ../..
 docker build -f backend/Dockerfile -t singlespasample/backend ../..
